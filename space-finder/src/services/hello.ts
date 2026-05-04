cd246c29901088628dc259a6da5b93c163cae172
Vivek <2105vivek@gmail.com>
2026-05-04 10:30:29 +0530

    use aws-sdk to list buckets

--- space-finder/src/services/hello.ts
+++ space-finder/src/services/hello.ts
@@ -1,10 +1,18 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import { v4 } from 'uuid';
+import { S3Client, ListBucketsCommand } from '@aws-sdk/client-s3';
+
+// initialise outside handler fxn so it can be reused
+const s3Client = new S3Client({}); 
 
 async function handler(event: APIGatewayProxyEvent, context: Context){
+
+  const command = new ListBucketsCommand({});
+  const listBucketsResult = (await s3Client.send(command)).Buckets;
+
   const response: APIGatewayProxyResult = {
     statusCode: 200,
-    body: JSON.stringify('Hello from lambda! this is teh id: ' + v4())
+    body: JSON.stringify('Hello from lambda! here are the buckets: ' + JSON.stringify(listBucketsResult))
   }
   console.log(event);
   return response;
